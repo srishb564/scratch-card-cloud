10807d1b1c02b246771a4b87fd8b31a510aee13b
srishb564 <srish564.banerjee@gmail.com>
2026-01-31 12:37:27 +0530

    App.py fixed

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db_connection():
-    return psycopg2.connect(DATABASE_URL, sslmode="require")
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
 def get_conn():
     return psycopg2.connect(os.environ["DATABASE_URL"])
